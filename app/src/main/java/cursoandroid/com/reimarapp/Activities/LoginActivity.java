1b7df7a01c1012082291d0bc0aa2abff29093891
Will Caleb <wilsonperepelecia@gmail.com>
2019-04-08 16:36:38 -0300

    tela de login funcionando

--- app/src/main/java/cursoandroid/com/reimarapp/Activities/LoginActivity.java
+++ app/src/main/java/cursoandroid/com/reimarapp/Activities/LoginActivity.java
@@ -2,15 +2,29 @@ package cursoandroid.com.reimarapp.Activities;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
 import cursoandroid.com.reimarapp.Classes.Usuario;
+import cursoandroid.com.reimarapp.Config.ConfiguracaoFirebase;
 import cursoandroid.com.reimarapp.R;
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,52 +34,58 @@ public class LoginActivity extends AppCompatActivity {
     private EditText edtSenha;
     private String permissao;
     private CheckBox checkBoxPreferencias;
+    private String usuarioLogado;
+
 
     private static final String ARQUIVO_PREFERENCIA ="ArqPref";
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        final DatabaseReference usuarios = databaseReference.child("usuarios");
 
         botaoLogar = findViewById(R.id.btn_logar);
         edtEmail = findViewById(R.id.edt_login_email);
         edtSenha = findViewById(R.id.edt_login_senha);
         checkBoxPreferencias = findViewById(R.id.checkBox_preferencias);
 
+        firebaseAuth = ConfiguracaoFirebase.getFirebaseAutenticacao();
+
         conferirPreferencias();
 
         botaoLogar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Usuario usuario = new Usuario();
 
                 String email = edtEmail.getText().toString();
                 String senha = edtSenha.getText().toString();
 
                 if(!email.isEmpty() && !senha.isEmpty()) {
-                    //Valida o login, determina no nível de acesso e envia a permissão através de um intent para a classe principal
+                    //Valida o login e abre a MainActivity
 
-                    Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                    Usuario usuario = new Usuario();
+                    usuario.setEmail(email);
+                    usuario.setSenha(senha);
 
-                    if (email.equals("admin") && senha.equals("admin")) {
-                        setPermissao("a");
-                        intent.putExtra("permissao", getPermissao());
-                        startActivity(intent);
-                        finish();
+                    firebaseAuth.signInWithEmailAndPassword(usuario.getEmail(), usuario.getSenha()
+                    ).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
 
-                    } else if (email.equals("tecnico") && senha.equals("tecnico")) {
-                        setPermissao("t");
-                        intent.putExtra("permissao", getPermissao());
-                        startActivity(intent);
-                        finish();
-                    }else { //caso os dados inseridos nos campos não sejam compatíveis exibe uma mensagem para o usuário,
-                            //limpa os campos e coloca o foco no campo email
-                        Toast.makeText(LoginActivity.this, "E-mail e/ou senha inválidos", Toast.LENGTH_SHORT).show();
-                        edtEmail.setText("");
-                        edtSenha.setText("");
-                        edtEmail.requestFocus();
-                    }
+                            if(task.isSuccessful()){
+                                abrirTelaPrincipal();
+                                finish();
+                            }else{
+                                Toast.makeText(LoginActivity.this, "Erro ao autenticar o usuário" + task.getException(), Toast.LENGTH_SHORT).show();
+                            }
+
+                        }
+                    });
+
                 }else if(email.isEmpty() || senha.isEmpty()){ //Se um dos campos não forem preenchidos exibe uma mensagem para o usuário e coloca o foco no campo não preenchido
                     Toast.makeText(LoginActivity.this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
                     if(email.isEmpty()) {
@@ -93,13 +113,18 @@ public class LoginActivity extends AppCompatActivity {
 
     }
     //confere se no arquivo ArqPref tem dados salvos, define o campo email com os dados do arquivo
-    //e mantém o checkbox ativado se tiverem os dados relevantes salvos
+    //    //e mantém o checkbox ativado se tiverem os dados relevantes salvos
     public void conferirPreferencias(){
         SharedPreferences sharedPreferences = getSharedPreferences(ARQUIVO_PREFERENCIA, 0);
         if(sharedPreferences.contains("email") && sharedPreferences.contains("senha")){
             edtEmail.setText(sharedPreferences.getString("email", ""));
             checkBoxPreferencias.setChecked(true);
         }
+    }
+
+    public void abrirTelaPrincipal(){
+        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 
     public String getPermissao() {
